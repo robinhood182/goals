8f43965e7167a00ab913e37907b4f8b864e704be
Robyn Navarro <rnavarro182@gmail.com>
2018-09-04 16:49:41 -0700

    initial scaff

---
+++ server/lib/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const errorHandler = require('./utils/error-handler');
+const ensureAuth = require('./auth/ensure-auth')();
+
+app.use(morgan('dev'));
+app.use(express.static('./public'));
+app.use(express.json());
+
+const auth = require('./routes/auth');
+const goals = require('./routes/goals');
+
+app.use('/api/auth', auth);
+app.use('/api/goals', ensureAuth, goals);
+
+app.use((req, res) => {
+    res.sendFile('index.html', { root: './public'} );
+});
+
+app.use(errorHandler());
+
+module.exports = app;
